a4bbcb380721946d85bd52b7b32798da3dc1682f
Matias Dubin <magondubi@gmail.com>
2025-04-21 09:42:14 -0300

    endpoints comunes completado

--- src/server.js
+++ src/server.js
@@ -1,20 +1,31 @@
-import express from "express"; 
-const app = express(); 
-const port = 6969; 
+import express from "express";
+import cors from "cors";
 
-app.get('/', (req,res)=>{
-    res.send("Hola a todos")
-})
-app.get('/saludar', (req, res) => { 
-    const nombre = req.query.nombre; 
-    
-    if (nombre) {
-        res.send(`Hello ${nombre}!!`);
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('¡Ya estoy respondiendo!');
+});
+
+app.get('/saludar/:nombre', (req, res) => {
+    const { nombre } = req.params;
+    res.status(200).send(`Hola ${nombre}`);
+});
+
+app.get('/validarfecha/:ano/:mes/:dia', (req, res) => {
+    const { ano, mes, dia } = req.params;
+    const fecha = new Date(`${ano}-${mes}-${dia}`);
+    if (isNaN(fecha.getTime())) {
+        res.status(400).send('Fecha inválida');
     } else {
-        res.send('Hello randomUser!!');
+        res.status(200).send('Fecha válida');
     }
-}); 
+});
 
 app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`); 
+    console.log(`Example app listening on port ${port}`);
 });
